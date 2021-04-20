ab9cf761e12b24eb2edbab323a918ee5ee7ea1f8
viviRamos <financeiro@portalautopecas.com.br>
2021-04-20 08:26:33 -0300

    1st day

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Welcome } from "./src/pages/Welcome";
+
+export default function App() {
+  return <Welcome />;
+}
---
+++ src/components/Button.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TouchableOpacityProps,
+} from "react-native";
+import colors from "../styles/colors";
+
+interface ButtonProps extends TouchableOpacityProps {
+  title: string;
+}
+
+export function Button({ title }: ButtonProps) {
+  return (
+    <TouchableOpacity activeOpacity={0.7} style={styles.button}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonText: {
+    color: colors.white,
+  },
+});
