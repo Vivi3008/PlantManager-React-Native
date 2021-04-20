17abc88aa930799ceba6ed9d86187032bb2183f9
viviRamos <financeiro@portalautopecas.com.br>
2021-04-20 18:13:18 -0300

    include fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Welcome } from "./src/pages/Welcome";
+import React from 'react';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from '@expo-google-fonts/jost';
+import AppLoading from 'expo-app-loading';
+import { Welcome } from './src/pages/Welcome';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
   return <Welcome />;
 }
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,32 +1,36 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-import { Button } from "../components/Button";
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-import wateringImg from "../assets/watering.png";
-import colors from "../styles/colors";
+import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
 
 export function Welcome() {
-  const [visible, setVisible] = useState(false);
-
-  const handleVisible = () => {
-    visible ? setVisible(false) : setVisible(true);
-  };
-  //#missaoespacial
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        Gerencie {"\n"}
-        suas plantas {"\n"}
+        Gerencie {'\n'}
+        suas plantas {'\n'}
         de forma fácil
       </Text>
 
-      {visible && <Image source={wateringImg} style={styles.image} />}
+      <Image source={wateringImg} style={styles.image} resizeMode='contain' />
 
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
         sempre que precisar.
       </Text>
-      <Button title="Imagem" onPress={handleVisible} />
+
+      <TouchableOpacity activeOpacity={0.7} style={styles.button}>
+        <Feather name='chevron-right' style={styles.buttonIcon} />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -34,25 +38,37 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "space-between",
+    alignItems: 'center',
+    justifyContent: 'space-around',
   },
 
   title: {
     fontSize: 32,
-    fontWeight: "bold",
-    textAlign: "center",
+    fontWeight: 'bold',
+    textAlign: 'center',
     color: colors.heading,
     marginTop: 38,
   },
   subtitle: {
-    textAlign: "center",
+    textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
   },
   image: {
-    width: 292,
-    height: 284,
+    height: Dimensions.get('window').width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonIcon: {
+    color: colors.white,
+    fontSize: 32,
   },
 });
