8e6f01b3998fb114a139a9d66a7945867d19fe95
viviRamos <financeiro@portalautopecas.com.br>
2021-04-27 15:52:15 -0300

    configure plant select

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,14 +1,68 @@
 import { useRoute } from '@react-navigation/core';
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { EnviromentButton } from '../components/EnviromentButton';
+import React, { useEffect, useState } from 'react';
 
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { EnviromentButton } from '../components/EnviromentButton';
 import { Header } from '../components/Header';
+import { PlantCardPrimary } from '../components/PlantCardPrimary';
+
+import api from '../services/api';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface EnvironmentProps {
+  key: string;
+  title: string;
+}
+
+interface PlantProps {
+  id: string;
+  name: string;
+  about: string;
+  water_tips: string;
+  photo: string;
+  environments: [string];
+  frequency: {
+    times: number;
+    repeat_every: string;
+  };
+}
+
 export function PlantSelect() {
   const { params } = useRoute();
+  const [enviroments, setEnviroments] = useState<EnvironmentProps[]>([]);
+  const [plants, setPlants] = useState<PlantProps[]>([]);
+  const [enviromentSelected, setEnviromentSelected] = useState('all');
+
+  const handleEnviromentSelected = (key: string) => {
+    setEnviromentSelected(key);
+  };
+
+  useEffect(() => {
+    async function fetchEnvironment() {
+      const { data } = await api.get(
+        'plants_environments?_sort=title&_order=asc'
+      );
+      setEnviroments([
+        {
+          key: 'all',
+          title: 'Todos',
+        },
+        ...data,
+      ]);
+    }
+
+    fetchEnvironment();
+  }, []);
+
+  useEffect(() => {
+    async function fetchPlants() {
+      const { data } = await api.get('plants?_sort=name&_order=asc');
+      setPlants(data);
+    }
+
+    fetchPlants();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -18,7 +72,31 @@ export function PlantSelect() {
         <Text style={styles.title}>Em qual ambiente</Text>
         <Text style={styles.subtitle}>você quer colocar sua planta?</Text>
       </View>
-      <EnviromentButton title='Cozinha' active />
+
+      <View>
+        <FlatList
+          data={enviroments}
+          renderItem={({ item }) => (
+            <EnviromentButton
+              title={item.title}
+              active={item.key === enviromentSelected}
+              onPress={() => handleEnviromentSelected(item.key)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.envinronmentList}
+        />
+      </View>
+
+      <View style={styles.plants}>
+        <FlatList
+          data={plants}
+          renderItem={({ item }) => <PlantCardPrimary data={item} />}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+        />
+      </View>
     </View>
   );
 }
@@ -43,5 +121,17 @@ const styles = StyleSheet.create({
   },
   header: {
     paddingHorizontal: 30,
+  },
+  envinronmentList: {
+    height: 40,
+    justifyContent: 'center',
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginVertical: 32,
+  },
+  plants: {
+    flex: 1,
+    paddingHorizontal: 32,
+    justifyContent: 'center',
   },
 });
